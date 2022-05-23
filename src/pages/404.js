c00dde51153666443945296b55a75a72dd8b9241
gaurabmahat <gaurab.mahat.work@gmail.com>
2022-05-23 15:46:21 +0300

    created a 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from './components/Layout'
+
+export default function PageNotFound() {
+  return (
+    <Layout>
+        <h1>404</h1>
+        <p>Sorry, that page does not exist!</p>
+    </Layout>
+  )
+}
