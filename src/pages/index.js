d9715ec5fe1f57d623902b4aafbbe7f3102d7039
gaurabmahat <gaurab.mahat.work@gmail.com>
2022-05-23 15:43:14 +0300

    create new pages, Navbar and Layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import Layout from "./components/Layout"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <section>
+        <div>
+          <h2>My Name is Gaurab Mahat</h2>
+        </div>
+      </section>
+    </Layout>
+  )
 }
